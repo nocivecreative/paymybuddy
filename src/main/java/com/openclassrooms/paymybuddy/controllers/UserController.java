5b93f0142c8279ec999fea0195acb99049712a52
nocivecreative <nocivecreative@gmail.com>
2026-01-14 09:29:25 +0100

    création des routes "user"

--- src/main/java/com/openclassrooms/paymybuddy/controllers/UserController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.openclassrooms.paymybuddy.dto.request.SignupRequestDTO;
 import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.service.UserService;
 
@@ -17,29 +18,44 @@ public class UserController {
 
     private final UserService userService;
 
+    // Test GET
     @GetMapping("/listusers")
     public String users(Model model) {
         model.addAttribute("users", userService.listAllUserForTest());
         return "list-users"; // list-users.html
     }
 
-    // Affichage du formulaire
+    // Affichage du formulaire d'inscription
     @GetMapping("/signup")
     public String showSignupForm(Model model) {
         model.addAttribute("user", new User());
         return "signup";
     }
 
-    // Soumission du formulaire
+    // Soumission du formulaire d'inscription
     @PostMapping("/signup")
-    public String processSignup(@ModelAttribute User user) {
-        userService.create(user);
+    public String processSignup(@ModelAttribute SignupRequestDTO signupRequest) {
+        userService.createFromSignup(signupRequest);
         return "redirect:/signup-success";
     }
 
+    // Confirmation d'inscription OK
     @GetMapping("/signup-success")
     public String signupSuccess() {
         return "signup-success";
+    }
+
+    // Vue du profil perso
+    @GetMapping("/profil")
+    public String profil() {
+        return "profil";
+
+    }
+    
+    // Edition du profil perso
+    @GetMapping("/profil/edit")
+    public String profilEdit() {
+        return "profil-edit";
     }
 
 }
