655f6bfb86fa8a9dfcac511f1fcb85ecf756621b
nocivecreative <nocivecreative@gmail.com>
2026-01-19 14:53:07 +0100

    Ajout méthode findByUser

--- src/main/java/com/openclassrooms/paymybuddy/repository/UserRelationRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/UserRelationRepository.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.paymybuddy.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.openclassrooms.paymybuddy.model.User;
@@ -9,5 +11,7 @@ import com.openclassrooms.paymybuddy.model.UserRelationId;
 public interface UserRelationRepository extends JpaRepository<UserRelation, UserRelationId> {
 
     boolean existsByUserAndFriend(User user, User friend);
+
+    List<UserRelation> findByUser(User user);
 
 }
