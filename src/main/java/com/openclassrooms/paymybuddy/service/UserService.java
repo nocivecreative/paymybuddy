3df5731e0d302de36015b35fc454a8281669364b
nocivecreative <nocivecreative@gmail.com>
2026-01-19 17:29:06 +0100

    Implémentation de la méthode doTranscation

--- src/main/java/com/openclassrooms/paymybuddy/service/UserService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/UserService.java
@@ -1,16 +1,20 @@
 package com.openclassrooms.paymybuddy.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.openclassrooms.paymybuddy.dto.response.RelationDTO;
 import com.openclassrooms.paymybuddy.exceptions.UserNotFoundException;
+import com.openclassrooms.paymybuddy.model.Transaction;
 import com.openclassrooms.paymybuddy.model.User;
 import com.openclassrooms.paymybuddy.model.UserRelation;
+import com.openclassrooms.paymybuddy.repository.TransactionsRepository;
 import com.openclassrooms.paymybuddy.repository.UserRelationRepository;
 import com.openclassrooms.paymybuddy.repository.UserRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -19,6 +23,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserRelationRepository userRelationRepository;
+    private final TransactionsRepository transactionRepository;
 
     public List<User> listAllUserForTest() { // TODO remove après tests
         return userRepository.findAll();
@@ -77,6 +82,33 @@ public class UserService {
                         relation.getFriend().getUsername(),
                         relation.getFriend().getEmail()))
                 .toList();
+    }
+
+    /**
+     * Effectue une transaction entre l'uutilisateur connecté, et l'utilisateur
+     * cible choisi.
+     * 
+     * @param currentUserId Utilisateur connecté
+     * @param idFriend      Utilisateur cible
+     * @param amount        Somme à transferer
+     * @param description   Texte de description pour la transacrion
+     * 
+     * @throws UserNotFoundException Si l'utilisateur (source ou cible) n'est pas
+     *                               trouvé
+     */
+    @Transactional
+    public void doTranscation(int currentUserId, int idFriend, BigDecimal amount, String description) {
+
+        User source = userRepository.findById(currentUserId)
+                .orElseThrow(() -> new UserNotFoundException("Utilisateur source introuvable : ID=" + currentUserId));
+
+        User friend = userRepository.findById(idFriend)
+                .orElseThrow(() -> new UserNotFoundException("Utilisateur cible introuvable : ID=" + idFriend));
+
+        Transaction transaction = new Transaction(source, friend, amount, description);
+
+        transactionRepository.save(transaction);
+
     }
 
 }
