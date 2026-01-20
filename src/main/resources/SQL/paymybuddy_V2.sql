16cecb433417ffb2b30389aca3f6ae50265c35fa
nocivecreative <nocivecreative@gmail.com>
2026-01-20 11:25:25 +0100

    Correction du lenght du password - incompatible avec BCrypt

--- src/main/resources/SQL/paymybuddy_V2.sql
+++ src/main/resources/SQL/paymybuddy_V2.sql
@@ -17,7 +17,7 @@ CREATE TABLE `paymybuddy`.`users` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'Id de l''utilisateur',
     `username` VARCHAR(50) NOT NULL,
     `email` VARCHAR(100) NOT NULL,
-    `password` VARCHAR(30) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
     PRIMARY KEY (`id`),
