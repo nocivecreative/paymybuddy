8cd121ed874933b7585891e179bacd7e84040b18
nocivecreative <nocivecreative@gmail.com>
2026-01-21 10:17:31 +0100

    MAJRrelationService suite passage en @ManyToMany

--- src/main/java/com/openclassrooms/paymybuddy/service/RelationService.java
+++ src/main/java/com/openclassrooms/paymybuddy/service/RelationService.java
@@ -8,8 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.openclassrooms.paymybuddy.dto.response.RelationDTO;
 import com.openclassrooms.paymybuddy.exceptions.UserNotFoundException;
 import com.openclassrooms.paymybuddy.model.User;
-import com.openclassrooms.paymybuddy.model.UserRelation;
-import com.openclassrooms.paymybuddy.repository.UserRelationRepository;
 import com.openclassrooms.paymybuddy.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
@@ -22,19 +20,18 @@ import lombok.AllArgsConstructor;
 public class RelationService implements RelationManagementInterface {
 
     private final UserRepository userRepository;
-    private final UserRelationRepository userRelationRepository;
 
     /**
      * Ajoute une relation entre l'utilisateur connecté à l'application et un
      * utilisateur enregistré via son adresse mail
-     * 
+     *
      * <li>Vérifie si les deux utilisateuur existent
      * <li>Empêche de s'ajouter soi-même
      * <li>Vérifie si la raletion existe déjà <br>
-     * 
+     *
      * @param currentUserId id de l'utilisateur connecté
      * @param friendEmail   adresse mail de l'utilisateur à ajouter
-     * 
+     *
      * @throws UserNotFoundException    si l'utilisateur n'est pas trouvé
      * @throws IllegalArgumentException gère les autres erreurs
      */
@@ -52,18 +49,18 @@ public class RelationService implements RelationManagementInterface {
             throw new IllegalArgumentException("Impossible de s'ajouter soi-même");
         }
 
-        if (userRelationRepository.existsByUserAndFriend(currentUser, friend)) {
+        if (currentUser.getFriends().contains(friend)) {
             throw new IllegalStateException("Relation déjà existante");
         }
 
-        UserRelation relation = new UserRelation(currentUser, friend);
-        userRelationRepository.save(relation);
+        currentUser.getFriends().add(friend);
+        userRepository.save(currentUser);
 
     }
 
     /**
      * Récupère la liste des relations existantes de l'utilisateur connecté
-     * 
+     *
      * @param currentUserId l'id de l'utilisaateur connecté
      * @return La liste des realtions existantes
      */
@@ -72,13 +69,11 @@ public class RelationService implements RelationManagementInterface {
         User user = userRepository.findById(currentUserId)
                 .orElseThrow(() -> new UserNotFoundException("Utilisateur introuvable"));
 
-        List<UserRelation> relationsList = userRelationRepository.findByUserWithFriend(user);
-
-        return relationsList.stream()
-                .map(relation -> new RelationDTO(
-                        relation.getFriend().getId(),
-                        relation.getFriend().getUsername(),
-                        relation.getFriend().getEmail()))
+        return user.getFriends().stream()
+                .map(friend -> new RelationDTO(
+                        friend.getId(),
+                        friend.getUsername(),
+                        friend.getEmail()))
                 .toList();
     }
 
