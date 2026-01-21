09ad8268d6aa8f894ada000661f6c3e5560ede7b
nocivecreative <nocivecreative@gmail.com>
2026-01-21 13:53:39 +0100

    Amélioration déclaration DTO's

--- src/main/java/com/openclassrooms/paymybuddy/dto/request/NewPassDTO.java
+++ src/main/java/com/openclassrooms/paymybuddy/dto/request/NewPassDTO.java
@@ -2,15 +2,19 @@ package com.openclassrooms.paymybuddy.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-@RequiredArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewPassDTO {
 
     @NotBlank(message = "Le mot de passe est obligatoire")
     @Size(min = 12, message = "Minimum 12 caracteres")
-    private final String password;
+    private String password;
 
 }
--- src/main/java/com/openclassrooms/paymybuddy/dto/request/NewUserDTO.java
+++ src/main/java/com/openclassrooms/paymybuddy/dto/request/NewUserDTO.java
@@ -3,23 +3,27 @@ package com.openclassrooms.paymybuddy.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@RequiredArgsConstructor
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewUserDTO {
 
     @NotBlank(message = "Le nom d'utilisateur est obligatoire")
     @Size(min = 3, max = 50, message = "Entre 3 et 50 caracteres")
-    private final String username;
+    private String username;
 
     @NotBlank(message = "L'email est obligatoire")
     @Email(message = "Format email invalide")
-    private final String email;
+    private String email;
 
     @NotBlank(message = "Le mot de passe est obligatoire")
     @Size(min = 12, message = "Minimum 12 caracteres")
-    private final String password;
+    private String password;
 
 }
