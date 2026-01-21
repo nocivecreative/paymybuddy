e06b5d7f088d1f01814e02cc7d1f3a264170bbde
nocivecreative <nocivecreative@gmail.com>
2026-01-21 11:56:05 +0100

    Maj utilitaire généraateur de hash BCrypt

--- src/main/java/com/openclassrooms/paymybuddy/utils/PasswordGenerator.java
+++ src/main/java/com/openclassrooms/paymybuddy/utils/PasswordGenerator.java
@@ -1,16 +1,26 @@
 package com.openclassrooms.paymybuddy.utils;
 
+import java.io.Console;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
- * Classe utilitaire pour générer un pass encrypté avec BCrypt
- * 
- * Run Java pour exec
- * 
+ * Classe utilitaire pour générer un hash BCrypt à partir d'un mot de passe
+ *
+ * Exécuter depuis un terminal externe (pas dans l'IDE)
  */
 public class PasswordGenerator {
     public static void main(String[] args) {
+        Console console = System.console();
+
+        System.out.println("Utilitaire de génération de hash BCrypt");
+        char[] passwordChars = console.readPassword("Entrez le mot de passe : ");
+        String password = new String(passwordChars);
+
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("user"));
+        System.out.println("Hash BCrypt correspondant : ");
+        System.out.println(encoder.encode(password));
+
+        java.util.Arrays.fill(passwordChars, ' ');
     }
 }
