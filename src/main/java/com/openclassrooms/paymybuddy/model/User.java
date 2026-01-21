25e96a12c7a8c3de2f7cc88b4efcce4b35235efb
nocivecreative <nocivecreative@gmail.com>
2026-01-21 10:13:01 +0100

    Passage en @ManyToMany sur User

--- src/main/java/com/openclassrooms/paymybuddy/model/User.java
+++ src/main/java/com/openclassrooms/paymybuddy/model/User.java
@@ -1,12 +1,18 @@
 package com.openclassrooms.paymybuddy.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -37,6 +43,14 @@ public class User {
 
     @Column(name = "updated_at", insertable = false, updatable = false)
     private LocalDateTime updatedAt;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "users_relations",
+            joinColumns = @JoinColumn(name = "id_user"),
+            inverseJoinColumns = @JoinColumn(name = "id_friend")
+    )
+    private List<User> friends = new ArrayList<>();
 
     public User(int id, String username, String email, String password) {
         this.id = id;
